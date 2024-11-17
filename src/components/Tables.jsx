a3d493bc699bbc0bc0d35bb29bc5a04f935ef70f
colowill <willminor64@gmail.com>
2024-11-16 22:32:07 -0500

    init commit, scraper functioning!

---
+++ src/components/Tables.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const name = document.querySelector('.')
+
+function Tables() {
+    return ( 
+    <div className="flex-col">
+        <div className="flex-row justify-between items-center ">
+
+            <h2 
+            className="text-3xl flex-grow font-bold">
+                Gargoyle AI
+            </h2>
+
+            <form className="flex-col">   
+                <div className="mb-6 w-1/3">
+                    <label
+                    className="block mb-2 justify-between text-align:left text-sm font-medium text-gray-900 w-50">
+                        hey
+                    </label>  
+                </div>
+            </form>
+        </div>
+    </div>
+    )
+}
+
+export default Tables
---
+++ src/utils/Scraper.js
@@ -0,0 +1,25 @@
+import puppeteer from 'puppeteer';
+
+const url = 'http://www.wxyc.info/playlists/recent'
+
+const scrapeInfo = async() => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    const musicData = await page.evaluate(() => {
+        const rows = Array.from(document.querySelectorAll('table:nth-of-type(2) tr[bgcolor="#F3F3F3"]'));
+        return rows.map(row => {
+
+            const artist = row.querySelector('td:nth-child(2)').textContent;
+            const song = row.querySelector('td:nth-child(3)').textContent;
+            const album = row.querySelector('td:nth-child(4)').textContent;
+            return { artist, song, album };
+        }).filter(item => item.artist && item.song && item.album);
+    });
+
+    console.log(musicData);
+    await browser.close()
+}
+
+scrapeInfo('http://www.wxyc.info/playlists/recent');
